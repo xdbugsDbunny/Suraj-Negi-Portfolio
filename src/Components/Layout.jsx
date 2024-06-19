22bb7ff1425f9dfb0d7e0678d8f57c22321feae3
Suraj Singh Negi <104298735+xdbugsDbunny@users.noreply.github.com>
2024-06-19 16:59:05 +0530

    bg site

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,10 +3,11 @@ import NavBar from "./NavBar";
 import Header from "./Header";
 import TopLeftImg from "./TopLeftImg";
 import LeftSideBar from "./LeftSideBar";
-import site from '../assets/site.svg'
+import site from '../assets/site.svg';
+
 const Layout = ({ children }) => {
   return (
-    <div className="page text-white bg-cover bg-no-repeat font-sora relative" style={{backgroundImage:`url(${site})`}}>
+    <div className="page text-white bg-cover bg-no-repeat font-sora relative" style={{backgroundImage: `url(${site})`}}>
       <LeftSideBar />
       <TopLeftImg />
       <NavBar />
